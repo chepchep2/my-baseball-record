8ed795ed8ce72e89b38d77bd8bfa830d0c15c647
chepchep2 <kajer102@gmail.com>
2026-03-18 17:12:57 +0900

    feat: Spring boot 백엔드 프로젝트 골격 추가

---
+++ backend/src/main/java/com/chepchep2/baseballrecord/MyBaseballRecordBackendApplication.java
@@ -0,0 +1,13 @@
+package com.chepchep2.baseballrecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBaseballRecordBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBaseballRecordBackendApplication.class, args);
+	}
+
+}
